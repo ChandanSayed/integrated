dc9a7605fc91507f6b922a36389af37489a63eb7
chandansayed <hangzhou635@gmail.com>
2023-12-10 15:01:24 +0600

    Form validation added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,30 @@
 import { useAppContext } from '@/context/AppContext';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
 
 const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
   const form = useRef();
+  const [formData, setFormData] = useState({
+    fName: '',
+    lName: '',
+    cName: '',
+    jobTitle: '',
+    phoneNumber: '',
+    email: '',
+    country: '',
+    message: ''
+  });
+  const [errors, setErrors] = useState([]);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
   const inputStyle = { border: '1px solid rgba(208, 213, 221, 0.15)', background: 'rgba(255, 255, 255, 0.05)', boxShadow: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)', direction: arabicLanguage ? 'rtl' : 'ltr' };
 
   function closeContact() {
@@ -12,29 +32,69 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
     setShowContact(false);
   }
 
+  function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
   const sendEmail = e => {
     e.preventDefault();
+    let formErrors = {};
+    if (formData.fName.trim() === '') {
+      formErrors.fName = 'First Name is required';
+    }
+    if (formData.lName.trim() === '') {
+      formErrors.lName = 'Last Name is required';
+    }
+    if (formData.cName.trim() === '') {
+      formErrors.cName = 'Company Name is required';
+    }
+    if (formData.jobTitle.trim() === '') {
+      formErrors.jobTitle = 'Job Title is required';
+    }
+    if (formData.phoneNumber.trim() === '') {
+      formErrors.phoneNumber = 'Phone Number is required';
+    }
+    if (formData.email.trim() === '') {
+      formErrors.email = 'Email is required';
+    }
+    if (formData.email.trim() !== '' && !validateEmail(formData.email)) {
+      formErrors.email = 'Email must be a valid Email';
+    }
+    if (formData.country.trim() === '') {
+      formErrors.country = 'Country Name is required';
+    }
+    if (formData.message.trim() === '') {
+      formErrors.message = 'Message Name is required';
+    }
 
-    emailjs.sendForm('service_e0yblnd', 'template_cfli0ks', form.current, 'tmF67ljka4s6SYzJ0').then(
-      result => {
-        console.log(result.text);
-        Swal.fire({
-          title: 'Good job!',
-          text: 'Your message sent successfully!',
-          icon: 'success'
-        });
-      },
-      error => {
-        console.log(error.text);
-      }
-    );
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      console.log('Form submitted:', formData);
+
+      emailjs.sendForm('service_e0yblnd', 'template_cfli0ks', form.current, 'tmF67ljka4s6SYzJ0').then(
+        result => {
+          console.log(result.text);
+          Swal.fire({
+            title: 'Good job!',
+            text: 'Your message sent successfully!',
+            icon: 'success'
+          });
+          closeContact();
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+    }
   };
 
   return (
     <>
       <div className={`fixed inset-0 z-20 bg-black bg-opacity-50 ${showContact ? '' : 'hidden'} `}></div>
       <div className={`max-w-[823px] w-full lg:rounded-lg mx-auto text-center fixed z-50 left-1/2 transform -translate-x-1/2 top-0 bottom-0 py-20 backdrop-blur-[10px] px-4 lg:px-16 overflow-y-scroll custom-scrollbar ${showContact ? '' : 'hidden'}`}>
-        <div className="sticky right-0 top-0 lg:top-12 pr-0 w-full">
+        <div className={`sticky right-0 top-0 lg:top-12 pr-0 w-[18px]${arabicLanguage ? '' : 'ml-auto'}`}>
           <svg onClick={closeContact} className="cursor-pointer ml-auto" xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
             <path d="M15.4517 17.5117C15.7343 17.8146 16.2089 17.8309 16.5117 17.5483C16.8146 17.2657 16.8309 16.7911 16.5483 16.4883L10.0259 9.5L16.5483 2.51174C16.8309 2.20893 16.8146 1.73433 16.5117 1.45171C16.2089 1.16908 15.7343 1.18545 15.4517 1.48826L9 8.40081L2.54829 1.48826C2.26567 1.18545 1.79107 1.16908 1.48826 1.45171C1.18545 1.73433 1.16908 2.20893 1.45171 2.51174L7.97409 9.5L1.45171 16.4883C1.16908 16.7911 1.18545 17.2657 1.48826 17.5483C1.79107 17.8309 2.26567 17.8146 2.54829 17.5117L9 10.5992L15.4517 17.5117Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
@@ -45,18 +105,20 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
         <p className="text-xs lg:text-lg text-white mx-auto text-center pb-[30px] lg:pb-[60px]">{arabicLanguage ? 'نحن نحب أن نسمع منك. يرجى ملء هذا النموذج.' : 'We’d love to hear from you. Please fill out this form.'} </p>
         <form ref={form} onSubmit={sendEmail} action="#" className={`form flex flex-col gap-[30px] ${arabicLanguage ? 'text-right' : ''}`}>
           <div className="flex flex-col gap-[30px] lg:gap-6">
-            <div className={`flex flex-col -[30px] ${arabicLanguage ? 'lg:flex-row-reverse' : 'lg:flex-row gap'}`}>
+            <div className={`flex flex-col gap-[30px] ${arabicLanguage ? 'lg:flex-row-reverse' : 'lg:flex-row gap'}`}>
               <div className="flex flex-col gap-1.5 items-start flex-1">
                 <label htmlFor="fName" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'الاسم الأول' : 'First name'}
                 </label>
-                <input style={inputStyle} type="text" id="fName" name="fName" placeholder={`${arabicLanguage ? 'الاسم الأول' : 'First name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input style={inputStyle} onChange={handleChange} value={formData.fName} type="text" id="fName" name="fName" placeholder={`${arabicLanguage ? 'الاسم الأول' : 'First name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                {errors.fName && <p className="text-red-600 text-xs mt-1">{errors.fName}</p>}
               </div>
               <div className="flex flex-col gap-1.5 items-start flex-1">
                 <label htmlFor="lName" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'اسم العائلة' : 'Last Name'}
                 </label>
-                <input style={inputStyle} type="text" id="lName" name="lName" placeholder={`${arabicLanguage ? 'اسم العائلة' : 'Last Name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input style={inputStyle} type="text" id="lName" onChange={handleChange} value={formData.lName} name="lName" placeholder={`${arabicLanguage ? 'اسم العائلة' : 'Last Name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                {errors.lName && <p className="text-red-600 text-xs mt-1">{errors.lName}</p>}
               </div>
             </div>
             <div className={`flex flex-col  gap-[30px] ${arabicLanguage ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
@@ -64,13 +126,15 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
                 <label htmlFor="cName" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'اسم الشركة' : 'Company Name'}
                 </label>
-                <input style={inputStyle} type="text" id="cName" name="cName" placeholder={`${arabicLanguage ? 'اسم الشركة' : 'Company Name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input style={inputStyle} onChange={handleChange} value={formData.cName} type="text" id="cName" name="cName" placeholder={`${arabicLanguage ? 'اسم الشركة' : 'Company Name'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                {errors.cName && <p className="text-red-600 text-xs mt-1">{errors.cName}</p>}
               </div>
               <div className="flex flex-col gap-1.5 items-start flex-1">
-                <label htmlFor="jTitle" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
+                <label htmlFor="jobTitle" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'مسمى وظيفي' : 'Job Title'}
                 </label>
-                <input style={inputStyle} type="text" id="jTitle" name="jTitle" placeholder={`${arabicLanguage ? 'مسمى وظيفي' : 'Job Title'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input style={inputStyle} onChange={handleChange} value={formData.jobTitle} type="text" id="jobTitle" name="jobTitle" placeholder={`${arabicLanguage ? 'مسمى وظيفي' : 'Job Title'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                {errors.jobTitle && <p className="text-red-600 text-xs mt-1">{errors.jobTitle}</p>}
               </div>
             </div>
             <div className={`flex flex-col  gap-[30px] ${arabicLanguage ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
@@ -78,13 +142,15 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
                 <label htmlFor="pNumber" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'رقم التليفون' : 'Phone number'}
                 </label>
-                <input style={{ border: '1px solid rgba(208, 213, 221, 0.15)', background: 'rgba(255, 255, 255, 0.05)', boxShadow: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)', direction: 'ltr' }} type="text" id="pNumber" name="pNumber" placeholder="+1 (555) 000-0000" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-left' : ''} `} />
+                <input style={{ border: '1px solid rgba(208, 213, 221, 0.15)', background: 'rgba(255, 255, 255, 0.05)', boxShadow: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)', direction: 'ltr' }} onChange={handleChange} value={formData.phoneNumber} type="text" id="pNumber" name="phoneNumber" placeholder="+1 (555) 000-0000" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-left' : ''} `} />
+                {errors.phoneNumber && <p className="text-red-600 text-xs mt-1">{errors.phoneNumber}</p>}
               </div>
               <div className="flex flex-col gap-1.5 items-start flex-1">
                 <label htmlFor="email" className={`font-Gilroy font-medium text-sm text-white ${arabicLanguage ? 'text-right block w-full' : ''}`}>
                   {arabicLanguage ? 'بريد إلكتروني' : 'Email'}
                 </label>
-                <input style={inputStyle} type="text" id="email" name="email" placeholder={`${arabicLanguage ? 'you@company.com' : 'you@company.com'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input style={inputStyle} type="text" onChange={handleChange} value={formData.email} id="email" name="email" placeholder={`${arabicLanguage ? 'you@company.com' : 'you@company.com'}`} className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                {errors.email && <p className="text-red-600 text-xs mt-1">{errors.email}</p>}
               </div>
             </div>
             <div className="flex flex-col lg:flex-row gap-[30px]">
@@ -93,8 +159,9 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
                   {arabicLanguage ? 'دولة' : 'Country'}
                 </label>
                 {/* <input style={inputStyle} type="text" id="pNumber" name="pNumber" placeholder="+1 (555) 000-0000" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage?'text-right':''} `} /> */}
-                <input type="text" style={inputStyle} name="Country" id="Country" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
+                <input type="text" onChange={handleChange} value={formData.country} style={inputStyle} name="country" id="Country" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `} />
                 {/* <option value="Country">{arabicLanguage ? 'دولة' : 'Country'}</option> */}
+                {errors.country && <p className="text-red-600 text-xs mt-1">{errors.country}</p>}
               </div>
             </div>
             <div className="flex flex-col lg:flex-row gap-[30px]">
@@ -105,7 +172,8 @@ const Contact = ({ showContact, setShowContact, arabicLanguage }) => {
                 {/* <select style={inputStyle} name="Country" id="Country" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage?'text-right':''} `}>
                 <option value="Country">Country</option>
               </select> */}
-                <textarea style={inputStyle} name="message" id="message" cols="30" rows="5" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `}></textarea>
+                <textarea style={inputStyle} onChange={handleChange} value={formData.message} name="message" id="message" cols="30" rows="5" className={`p-[12px_10px_12px_16px] text-base text-white rounded-[8px] outline-transparent focus:border-transparent w-full ${arabicLanguage ? 'text-right' : ''} `}></textarea>
+                {errors.message && <p className="text-red-600 text-xs mt-1">{errors.message}</p>}
               </div>
             </div>
             <div className={`flex items-start ${arabicLanguage ? 'lg:flex-row-reverse' : ''}`}>
